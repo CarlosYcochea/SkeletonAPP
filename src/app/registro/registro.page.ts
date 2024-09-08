3e60e0d3d09c42ccbf7185daf860e11431656322
CarlosYcochea <134462386+CarlosYcochea@users.noreply.github.com>
2024-09-07 20:09:17 -0400

    cambios y jquery

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,25 +1,29 @@
-import { Component} from '@angular/core';
+import { Component,OnInit} from '@angular/core';
 import { Router } from '@angular/router';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
   styleUrls: ['./registro.page.scss'],
 })
-export class RegistroPage{
-  nombre: string = '';
-  email: string = '';
-  password: string = '';
+export class RegistroPage implements OnInit {
 
   constructor(private router: Router) { }
 
-  registro() {
-    if (this.nombre && this.email && this.password) {
-      alert('Registro exitoso');
-      this.router.navigate(['/login']);
-    } else {
-      alert('Por favor, complete todos los campos');
-    }
+  ngOnInit() {
+    $('#registroForm').on('submit', (e) =>{
+      e.preventDefault();
+      const nombre = $('#nombre').val();
+      const email = $('#email').val();
+      const password = $('#password').val();
+      
+      if(nombre && email && password) {
+        alert('Registro exitoso');
+        this.router.navigate(['/login']);
+      } else {
+        alert('Por favor, completa todos los campos.');
+      }
+    });
   }
-
 }
